358b3138b23eea3ee0d607f7c55e2687a5e9ae7b
Severin Spoerri <severin.spoerri@outlook.com>
2019-02-05 20:13:37 +0100

    refactor: changed file names

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Map from './components/Map'
+import locations from './data/locations';
+import Map from './components/Map';
 import './App.css';
 
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
